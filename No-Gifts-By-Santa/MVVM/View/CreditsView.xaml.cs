a744e7db745392e15d1083211224c73f569aff49
kallioderso <pascal.loesch.coburg@gmail.com>
2025-12-23 09:53:39 +0100

    Adding the Remaining Base structure of everything

---
+++ No-Gifts-By-Santa/MVVM/View/CreditsView.xaml.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace No_Gifts_By_Santa.MVVM.View;
+
+public partial class CreditsView : ContentPage
+{
+    public CreditsView()
+    {
+        InitializeComponent();
+    }
+}
--- No-Gifts-By-Santa/MVVM/ViewModel/MenuViewModel.cs
+++ No-Gifts-By-Santa/MVVM/ViewModel/MenuViewModel.cs
@@ -1,19 +1,36 @@
 ﻿using System.ComponentModel;
 using System.Runtime.CompilerServices;
 using System.Collections.Generic;
+using System.Windows.Input;
+using No_Gifts_By_Santa.MVVM.View;
 
 namespace No_Gifts_By_Santa.MVVM.ViewModel
 {
     public class MenuViewModel : INotifyPropertyChanged
     {
+        //Variables - Basic
         private double _buttonWidth;
         public double ButtonWidth
         {
             get => _buttonWidth;
             set => SetField(ref _buttonWidth, value);
         }
+        
+        //Variables - ICommands
+        public ICommand _levelView { get; }
+        public ICommand _creditsView { get; }
+        public ICommand _settingsView { get; }
+        public ICommand _tutorialView { get; }
 
-        //Ba
+        public MenuViewModel()
+        {
+            _levelView = new Command<Button>(LevelButton);
+            _creditsView = new Command<Button>(CreditsButton);
+            _settingsView = new Command<Button>(SettingsButton);
+            _tutorialView = new Command<Button>(TutorialButton);
+        }
+        
+        //Basic Functions for Changing the Buttons Width
         public void ChangeSize(double width, double height) => ResizingButtons(width, height);
         private void ResizingButtons(double width, double height)
         {
@@ -21,6 +38,40 @@ namespace No_Gifts_By_Santa.MVVM.ViewModel
             Console.WriteLine(height);
             ButtonWidth = width / 10;
             OnPropertyChanged(nameof(ButtonWidth));
+        }
+        
+        //Button Methods / ICommands
+        private async void LevelButton(Button button)
+        {
+            try
+            {
+                if (Application.Current?.MainPage?.Navigation != null)
+                    await Application.Current.MainPage.Navigation.PushAsync(new MVVM.View.LevelView(), true);
+            }catch (Exception){/*inactive*/}
+        }
+        private async void CreditsButton(Button button)
+        {
+            try
+            {
+                if (Application.Current?.MainPage?.Navigation != null)
+                    await Application.Current.MainPage.Navigation.PushAsync(new MVVM.View.CreditsView(), true);
+            }catch (Exception){/*inactive*/}
+        }
+        private async void SettingsButton(Button button)
+        {
+            try
+            {
+                if (Application.Current?.MainPage?.Navigation != null)
+                    await Application.Current.MainPage.Navigation.PushAsync(new MVVM.View.SettingsView(), true);
+            }catch (Exception){/*inactive*/}
+        }
+        private async void TutorialButton(Button button)
+        {
+            try
+            {
+                if (Application.Current?.MainPage?.Navigation != null)
+                    await Application.Current.MainPage.Navigation.PushAsync(new MVVM.View.TutorialView(), true);
+            }catch (Exception){/*inactive*/}
         }
         
         //Basic Logics for the ViewModel functions.
