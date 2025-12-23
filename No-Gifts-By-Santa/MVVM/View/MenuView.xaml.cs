ea918ef74fd3d5c7b11b0e6af489524f8c830172
kallioderso <pascal.loesch.coburg@gmail.com>
2025-12-23 09:30:36 +0100

    Setting up the basic stuff for our Menu Screen, where the buttons should change in width while resizing the window

---
+++ No-Gifts-By-Santa/MVVM/View/MenuView.xaml.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using No_Gifts_By_Santa.MVVM.ViewModel;
+
+namespace No_Gifts_By_Santa.MVVM.View;
+
+public partial class MenuView : ContentPage
+{
+    private MenuViewModel? _viewModel;
+    public MenuView()
+    {
+        InitializeComponent();
+        _viewModel = BindingContext as MenuViewModel;
+        this.SizeChanged += SizeChangedEvent;
+    }
+
+    void SizeChangedEvent(object? sender, EventArgs eventArgs) => _viewModel?.ChangeSize(this.Width, this.Height);
+}
---
+++ No-Gifts-By-Santa/MVVM/ViewModel/MenuViewModel.cs
@@ -0,0 +1,43 @@
+﻿using System.ComponentModel;
+using System.Runtime.CompilerServices;
+using System.Collections.Generic;
+
+namespace No_Gifts_By_Santa.MVVM.ViewModel
+{
+    public class MenuViewModel : INotifyPropertyChanged
+    {
+        private double _buttonWidth;
+        public double ButtonWidth
+        {
+            get => _buttonWidth;
+            set => SetField(ref _buttonWidth, value);
+        }
+
+        //Ba
+        public void ChangeSize(double width, double height) => ResizingButtons(width, height);
+        private void ResizingButtons(double width, double height)
+        {
+            Console.WriteLine(width);
+            Console.WriteLine(height);
+            ButtonWidth = width / 10;
+            OnPropertyChanged(nameof(ButtonWidth));
+        }
+        
+        //Basic Logics for the ViewModel functions.
+        public event PropertyChangedEventHandler? PropertyChanged;
+
+        protected virtual void OnPropertyChanged([CallerMemberName] string? propertyName = null)
+        {
+            PropertyChanged?.Invoke(this, new PropertyChangedEventArgs(propertyName));
+        }
+
+        protected bool SetField<T>(ref T field, T value, [CallerMemberName] string? propertyName = null)
+        {
+            if (EqualityComparer<T>.Default.Equals(field, value)) return false;
+            field = value;
+            OnPropertyChanged(propertyName);
+            return true;
+        }
+    }    
+}
+
